03236d9cb46df59ef22a9c66d22b3d94c10527af
Andreas Kvas <kvas@tugraz.at>
2022-01-26 14:36:34 +0100

    updated docs

--- docs/build/search_index.js
+++ docs/build/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"-","title":"-","text":"recursionfactors(maximum_degree)","category":"page"}]
+[{"location":"","page":"-","title":"-","text":"recursionfactors(maximum_degree)","category":"page"},{"location":"#MassChangeRepresentations.recursionfactors-Tuple{Any}","page":"-","title":"MassChangeRepresentations.recursionfactors","text":"recursionfactors(maximum_degree)\n\nCompute recursion factors for associated Legendre functions and spherical harmonics.\n\nThe factors are stored in a matrix of size n_textmax+1 times n_textmax+1.\n\n\n\n\n\n","category":"method"}]
 }
