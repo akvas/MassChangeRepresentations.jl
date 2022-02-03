2a04cc4247a998d7e1ff09983a9fc2c3cfbccfad
Andreas Kvas <kvas@tugraz.at>
2022-02-03 10:31:39 +0100

    restructured source files

--- src/MassChangeRepresentations.jl
+++ src/MassChangeRepresentations.jl
@@ -3,6 +3,7 @@ module MassChangeRepresentations
 include("data.jl")
 include("constants.jl")
 include("kernels.jl")
+include("representations.jl")
 include("grids.jl")
 include("spherical_harmonics.jl")
 
