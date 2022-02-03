07e91579522150ffa3ca49124f0ed0978ebb07eb
Andreas Kvas <kvas@tugraz.at>
2022-02-03 10:33:46 +0100

    new interface for kernel coefficients to take into account different earth parameters

--- src/kernels.jl
+++ src/kernels.jl
@@ -7,39 +7,54 @@ abstract type AnisotropicKernel <: Kernel end
 
 struct Potential <: IsotropicKernel end
 
-struct SurfaceDensity <: IsotropicKernel end
+struct SurfaceDensity <: IsotropicKernel
+    system
+end
 
-function coefficients(kernel::Potential, minimum_degree, maximum_degree)
+struct RadialDeformation <: IsotropicKernel
+    system
+end
+
+function coefficients(kernel::Potential, minimum_degree, maximum_degree, GM, R)
     kn = ones(maximum_degree + 1)
     kn[1:minimum_degree] .= 0
     return kn
 end
 
-function coefficients(kernel::SurfaceDensity, minimum_degree, maximum_degree; system="CE")
+function coefficients(kernel::SurfaceDensity, minimum_degree, maximum_degree, GM, R)
     kn = Vector{Float64}(undef, maximum_degree + 1)
     kn[1:minimum_degree] .= 0
     for n in minimum_degree:maximum_degree
-        kn[n + 1] = (1 + Constants.loadlovenumber(system, n)) / (2 * n + 1) * (4 * pi * Constants.IERS2010.G)
+        kn[n + 1] = (1 + Constants.loadlovenumber(kernel.system, n)) / (2 * n + 1) * (4 * pi * Constants.IERS2010.G) * R
     end
     return kn
 end
 
-function inverse_coefficients(kernel::IsotropicKernel, minimum_degree, maximum_degree)
-    kn = coefficients(kernel, minimum_degree, maximum_degree)
+function coefficients(kernel::RadialDeformation, minimum_degree, maximum_degree, GM, R)
+    kn = Vector{Float64}(undef, maximum_degree + 1)
+    kn[1:minimum_degree] .= 0
+    for n in minimum_degree:maximum_degree
+        kn[n + 1] = (1 + Constants.loadlovenumber(kernel.system, n)) / Constants.deformationlovenumbers(kernel.system, n)[1] * (GM / R^2)
+    end
+    return kn
+end
+
+function inverse_coefficients(kernel::IsotropicKernel, minimum_degree, maximum_degree, GM, R)
+    kn = coefficients(kernel, minimum_degree, maximum_degree, GM, R)
     for n in minimum_degree:maximum_degree
         kn[n + 1] = kn[n + 1] == 0.0 ? 0.0 : 1 / kn[n + 1]
     end
     return kn
 end
 
-function evaluate(kernel::IsotropicKernel, p, q, minimum_degree, maximum_degree)
-    kn = coefficients(kernel, minimum_degree, maximum_degree) .* sqrt.(2.0.*(0:maximum_degree).+1.0)
+function evaluate(kernel::IsotropicKernel, p, q, minimum_degree, maximum_degree, GM, R)
+    kn = coefficients(kernel, minimum_degree, maximum_degree, GM, R) .* sqrt.(2.0.*(0:maximum_degree).+1.0)
     factors = summationfactors(maximum_degree)
     legendre_summation(cosangle(p, q), factors, kn)
 end
 
-function evaluate(kernel::IsotropicKernel, psi::Number, minimum_degree, maximum_degree)
-    kn = coefficients(kernel, minimum_degree, maximum_degree) .* sqrt.(2.0.*(0:maximum_degree).+1.0)
+function evaluate(kernel::IsotropicKernel, psi::Number, minimum_degree, maximum_degree, GM, R)
+    kn = coefficients(kernel, minimum_degree, maximum_degree, GM, R) .* sqrt.(2.0.*(0:maximum_degree).+1.0)
     factors = summationfactors(maximum_degree)
     legendre_summation(cos(psi), factors, kn)
 end
